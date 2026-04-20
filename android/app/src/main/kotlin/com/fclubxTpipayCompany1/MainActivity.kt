9237a58004fd57abe1e83688ab6773ee9cda8bad
Venkatesh22 <venkateshrathod242002@gmail.com>
2026-04-20 12:41:41 +0530

    update package name

--- android/app/src/main/kotlin/com/fclubxTpipayCompany1/MainActivity.kt
+++ android/app/src/main/kotlin/com/fclubxTpipayCompany1/MainActivity.kt
@@ -1,4 +1,4 @@
-package fclubxTpipayCompany
+package com.fclubxTpipayCompany1
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\venra\OneDrive\Desktop\Tpipay\Fclubx\code\Fclubx-tpipay"
+export "FLUTTER_ROOT=/Users/venkateshrathod/develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/venkateshrathod/Code/fclubx/code/Fclubx-tpipay"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
